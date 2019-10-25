bfd943ac0e0d0ea1b1d44cc8c2a307469db17cfa
AndreJulio <andre3mxd@gmail.com>
2019-10-25 18:19:12 +0100

    some classes

---
+++ src/org/academiadecodigo/vimdiesels/shipwreck/Board.java
@@ -0,0 +1,18 @@
+package org.academiadecodigo.vimdiesels.shipwreck;
+
+public class Board {
+
+    Tile[][] board;
+    private int size;
+
+    public Board(int size) {
+        this.size = size;
+        this.board = new Tile[size][size];
+    }
+
+
+    
+
+
+
+}
---
+++ src/org/academiadecodigo/vimdiesels/shipwreck/Game.java
@@ -0,0 +1,15 @@
+package org.academiadecodigo.vimdiesels.shipwreck;
+
+public class Game {
+
+    private boolean isStarted;
+    private Player p1;
+    private Player p2;
+    private Board board;
+
+    public Game(Player p1, Player p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.board = new Board();
+    }
+}
