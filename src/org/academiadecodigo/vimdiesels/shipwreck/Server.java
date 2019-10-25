170174503651ebf726e455459120dd24128b5af8
Danones <diogo.sousa.uk@gmail.com>
2019-10-25 18:29:15 +0100

    Add methods to server

---
+++ src/org/academiadecodigo/vimdiesels/shipwreck/Server.java
@@ -0,0 +1,45 @@
+package org.academiadecodigo.vimdiesels.shipwreck;
+
+import org.academiadecodigo.bootcamp.Prompt;
+import org.academiadecodigo.bootcamp.scanners.menu.MenuInputScanner;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+    private ExecutorService threadPool;
+
+    //1st - start the threads and call the listening method
+    public void run(int port) throws IOException {
+
+        init(port);
+        threadPool = Executors.newCachedThreadPool();
+
+        while (true) {
+            listening();
+        }
+    }
+    //2nd - start the server with a welcome message
+    public void init(int port) throws IOException {
+
+        Prompt prompt = new Prompt(System.in, System.out);
+
+        String[] menuOptions = {"Log In", "Exit"};
+
+        MenuInputScanner opt = new MenuInputScanner(menuOptions);
+        opt.setMessage("=== Welcome to SHIP WRECK ===");
+        serverSocket = new ServerSocket(port);
+    }
+    //3rd - Server starts listening for players to join in
+    public void listening() throws IOException {
+
+        Socket playerSocket = serverSocket.accept();
+        System.out.println(Thread.activeCount() + "\n");
+       // threadPool.submit(new Player(playerSocket));
+    }
+}
