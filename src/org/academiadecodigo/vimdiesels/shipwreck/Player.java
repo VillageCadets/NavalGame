f962b6429197aba0bc1088f2d781707c05bb9fd7
Danones <diogo.sousa.uk@gmail.com>
2019-10-25 19:56:24 +0100

    edit Player

--- src/org/academiadecodigo/vimdiesels/shipwreck/Player.java
+++ src/org/academiadecodigo/vimdiesels/shipwreck/Player.java
@@ -1,12 +1,43 @@
 package org.academiadecodigo.vimdiesels.shipwreck;
 
-public class Player {
+import org.academiadecodigo.bootcamp.Prompt;
+import org.academiadecodigo.bootcamp.scanners.menu.MenuInputScanner;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.net.Socket;
+
+public class Player implements Runnable {
 
     private String username;
+    private Socket playerSocket;
+    private Server server;
+    private BufferedReader in;
+    private BufferedWriter out;
+
+    public Player(Server server, Socket playerSocket) {
+        this.server = server;
+        this.playerSocket = playerSocket;
 
 
-    public Player(String username) {
-        this.username = username;
+    }
+
+    @Override
+    public void run() {
+
+        while(!playerSocket.isClosed() && playerSocket != null){
+
+            String[] menuOptions = {"Play ", "Exit"};
+            Prompt prompt = new Prompt(System.in, System.out);
+            MenuInputScanner opt = new MenuInputScanner(menuOptions);
+            opt.setMessage("====== SHIP WRECK ======");
+
+            int answerIndex = prompt.getUserInput(opt);
+            //if (answerIndex == 1){
+
+            //}
+            System.out.println("User wants to " + menuOptions[answerIndex - 1] + "\n");
+        }
     }
 
 
