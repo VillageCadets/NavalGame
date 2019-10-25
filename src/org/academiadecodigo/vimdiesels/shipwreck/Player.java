301de0878bea23c63b624563ce6ede19a7220d3a
Danones <diogo.sousa.uk@gmail.com>
2019-10-25 20:53:37 +0100

    menu working on each player

--- src/org/academiadecodigo/vimdiesels/shipwreck/Player.java
+++ src/org/academiadecodigo/vimdiesels/shipwreck/Player.java
@@ -3,8 +3,7 @@ package org.academiadecodigo.vimdiesels.shipwreck;
 import org.academiadecodigo.bootcamp.Prompt;
 import org.academiadecodigo.bootcamp.scanners.menu.MenuInputScanner;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class Player implements Runnable {
@@ -15,28 +14,39 @@ public class Player implements Runnable {
     private BufferedReader in;
     private BufferedWriter out;
 
-    public Player(Server server, Socket playerSocket) {
+    public Player(Server server, Socket playerSocket) throws IOException {
         this.server = server;
         this.playerSocket = playerSocket;
 
+        this.in = new BufferedReader(new InputStreamReader(playerSocket.getInputStream()));
+        this.out = new BufferedWriter(new OutputStreamWriter(playerSocket.getOutputStream()));
 
     }
 
     @Override
     public void run() {
+        System.out.println("Player " + username + " started." );
+
+        try {
+            InputStream input = playerSocket.getInputStream();
+            PrintStream output = new PrintStream(playerSocket.getOutputStream());
 
         while(!playerSocket.isClosed() && playerSocket != null){
 
             String[] menuOptions = {"Play ", "Exit"};
-            Prompt prompt = new Prompt(System.in, System.out);
+            Prompt prompt = new Prompt(input, output);
             MenuInputScanner opt = new MenuInputScanner(menuOptions);
             opt.setMessage("====== SHIP WRECK ======");
 
             int answerIndex = prompt.getUserInput(opt);
-            //if (answerIndex == 1){
 
-            //}
+            if (answerIndex == 1){
+
+            }
             System.out.println("User wants to " + menuOptions[answerIndex - 1] + "\n");
+        }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
