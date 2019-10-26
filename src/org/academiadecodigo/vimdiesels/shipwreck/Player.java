2017c43b42b3401b14b5f7e7bc6e484a8295ca9e
Danones <diogo.sousa.uk@gmail.com>
2019-10-26 11:36:32 +0100

    master with directories

--- src/org/academiadecodigo/vimdiesels/shipwreck/Player.java
+++ src/org/academiadecodigo/vimdiesels/shipwreck/Player.java
@@ -13,38 +13,41 @@ public class Player implements Runnable {
     private Server server;
     private BufferedReader in;
     private BufferedWriter out;
+    private Player[] players;
 
     public Player(Server server, Socket playerSocket) throws IOException {
         this.server = server;
         this.playerSocket = playerSocket;
-
         this.in = new BufferedReader(new InputStreamReader(playerSocket.getInputStream()));
         this.out = new BufferedWriter(new OutputStreamWriter(playerSocket.getOutputStream()));
-
     }
 
     @Override
     public void run() {
-        System.out.println("Player " + username + " started." );
+        System.out.println("Player " + username + " started.");
 
         try {
             InputStream input = playerSocket.getInputStream();
             PrintStream output = new PrintStream(playerSocket.getOutputStream());
 
-        while(!playerSocket.isClosed() && playerSocket != null){
+            while (!playerSocket.isClosed() && playerSocket != null) {
 
-            String[] menuOptions = {"Play ", "Exit"};
-            Prompt prompt = new Prompt(input, output);
-            MenuInputScanner opt = new MenuInputScanner(menuOptions);
-            opt.setMessage("====== SHIP WRECK ======");
+                String[] menuOptions = {"Play ", "Exit"};
+                Prompt prompt = new Prompt(input, output);
+                MenuInputScanner opt = new MenuInputScanner(menuOptions);
 
-            int answerIndex = prompt.getUserInput(opt);
+                opt.setMessage("====== SHIP WRECK ======");
 
-            if (answerIndex == 1){
+                int answerIndex = prompt.getUserInput(opt);
 
+                if (answerIndex == 1) {
+                    if (players.length == 2) {
+                        Game game = new Game()
+                    }
+
+                }
+                System.out.println("User wants to " + menuOptions[answerIndex - 1] + "\n");
             }
-            System.out.println("User wants to " + menuOptions[answerIndex - 1] + "\n");
-        }
         } catch (IOException e) {
             e.printStackTrace();
         }
