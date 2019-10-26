12a74f5c1c98f694ef7849fcdc974b886e87f0df
Danones <diogo.sousa.uk@gmail.com>
2019-10-26 15:28:46 +0100

    changes to README

--- src/org/academiadecodigo/vimdiesels/shipwreck/ships/Ship.java
+++ src/org/academiadecodigo/vimdiesels/shipwreck/ships/Ship.java
@@ -1,12 +1,32 @@
 package org.academiadecodigo.vimdiesels.shipwreck.ships;
 
+import org.academiadecodigo.vimdiesels.shipwreck.board.Board;
+import org.academiadecodigo.vimdiesels.shipwreck.board.Tile;
+
 public class Ship {
 
-    private int size;
+    private int shipSize;
 
     public Ship(int size) {
 
-        this.size = size;
+        this.shipSize = size;
+    }
+
+    private void placeShip(Board board){
 
     }
+
+    private void shipWithinBoard(Board board, Tile firstTile ){
+
+        boolean insideBoard = true;
+
+        for (int i = 0; i < shipSize ; i++) {
+            //(if (!board.))
+        }
+    }
+
+    public int getShipSize() {
+        return shipSize;
+    }
 }
+
