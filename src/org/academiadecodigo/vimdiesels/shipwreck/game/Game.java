caec60c69452697de22553c1770dc50f55e4751b
Danones <diogo.sousa.uk@gmail.com>
2019-10-27 12:40:37 +0000

    Game implemented with board

--- src/org/academiadecodigo/vimdiesels/shipwreck/game/Game.java
+++ src/org/academiadecodigo/vimdiesels/shipwreck/game/Game.java
@@ -3,17 +3,24 @@ package org.academiadecodigo.vimdiesels.shipwreck.game;
 import org.academiadecodigo.vimdiesels.shipwreck.board.Board;
 import org.academiadecodigo.vimdiesels.shipwreck.board.Tile;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
 public class Game {
 
     private boolean onHold;
     private Player p1;
     private Player p2;
     private Board board;
+    private Player [] players;
+
 
     public Game(Player p1) {
         this.p1 = p1;
         this.board = new Board(20);
         onHold = true;
+        this.players = new Player[2];
         System.out.println("Waiting for players to join");
     }
 
@@ -21,6 +28,7 @@ public class Game {
         return onHold;
     }
 
+    //we start the game and call the method start
     public void init() {
         System.out.println("GAME INIT");
         onHold = false;
@@ -29,10 +37,28 @@ public class Game {
 
 
     private void showBoard(){
+
+        PrintWriter printWriter;
         this.board = new Board(10);
         board.init();
         board.drawBoard();
+
+        try {
+
+            for (Player p: players) {
+
+                printWriter = new PrintWriter(p.getPlayerSocket().getOutputStream());
+                printWriter.print("HEREEEEEEEEEEE");
+                printWriter.flush();
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
 
     private void start(){
 
@@ -44,5 +70,7 @@ public class Game {
 
     public void addPlayer(Player player) {
         p2 = player;
+        players [0] = p1;
+        players [1] = p2;
     }
 }
--- src/org/academiadecodigo/vimdiesels/shipwreck/game/Player.java
+++ src/org/academiadecodigo/vimdiesels/shipwreck/game/Player.java
@@ -80,4 +80,7 @@ public class Player {
         return null;
     }
 
+    public Socket getPlayerSocket() {
+        return playerSocket;
+    }
 }
